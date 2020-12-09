8bd539b545a034ee87c7b76ff54203ad43bb7044
Oleg Smarandi <48328325+Smarandii@users.noreply.github.com>
2020-12-09 10:02:48 +0300

    initial commit

---
+++ MyForm.h
@@ -0,0 +1,228 @@
+﻿//12. Дан двумерный массив размерности N*M, все элементы которого различны.
+//Удалить строки и столбцы, на пересечении которых находятся нулевые элементы.
+//Вывести получившуюся матрицу на экран
+
+
+#pragma once
+#include <iostream> 
+#include <sstream> 
+
+
+namespace CODINGlab3 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
+			this->dataGridView1->CellBorderStyle = System::Windows::Forms::DataGridViewCellBorderStyle::Sunken;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->ColumnHeadersVisible = false;
+			this->dataGridView1->Location = System::Drawing::Point(137, 12);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->Size = System::Drawing::Size(389, 307);
+			this->dataGridView1->TabIndex = 0;
+			this->dataGridView1->Visible = false;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(65, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(66, 20);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(65, 68);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(66, 20);
+			this->textBox2->TabIndex = 2;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 15);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(18, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"N:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(12, 71);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(17, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"K:";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 121);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(119, 41);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"Показать матрицу";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(286, 325);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(240, 89);
+			this->button3->TabIndex = 7;
+			this->button3->Text = L"Выполнить процедуру";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(538, 426);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox2->Text != "") {
+			int tmp = System::Convert::ToInt16(textBox2->Text);
+			dataGridView1->ColumnCount = tmp;
+		}
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	dataGridView1->Visible = true;
+}
+
+
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	int N, M, i, j;
+	bool zero_exist = true;
+	N = dataGridView1->RowCount;
+	M = dataGridView1->ColumnCount;
+	i = 0;
+	j = 0;
+	
+	while (zero_exist){
+
+		// Находим самый крайний ноль
+		int zero_i = -1, zero_j = -1;
+		for (i = 0; i < N; i++)
+			for (j = 0; j < M; j++)
+				if (System::Convert::ToInt16(dataGridView1[i,j]->Value) == 0)
+				{
+					zero_i = i; zero_j = j;
+				}
+		if (zero_i == -1 && zero_j == -1) break; 
+
+		for (i = 0; i < N; i++)
+		{
+			for (j = zero_j; j < M - 1; j++)
+				dataGridView1[i,j]->Value = dataGridView1[i,j + 1]->Value;
+		}
+
+		for (i = 0; i < N; i++)
+		{
+			for (j = zero_j; j < M - 1; j++)
+				dataGridView1[j,i]->Value = dataGridView1[j + 1,i]->Value;
+		}
+		N--;
+		M--;
+	}
+	dataGridView1->RowCount = N;
+	dataGridView1->ColumnCount = M;
+	
+}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	if (textBox1->Text != "")
+	{
+	int tmp = System::Convert::ToInt16(textBox1->Text);
+	dataGridView1->RowCount = tmp;
+	}
+}
+};
+}
